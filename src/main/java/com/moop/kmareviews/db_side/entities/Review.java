d8926ddecd28173fe3dd278dc7d30937048f1328
Karina Cherednyk <karina0cher@gmail.com>
2020-11-29 16:30:10 +0200

    fix date bug

--- src/main/java/com/moop/kmareviews/db_side/entities/Review.java
+++ src/main/java/com/moop/kmareviews/db_side/entities/Review.java
@@ -36,6 +36,6 @@ public class Review{
 
 
     @Column(updatable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "hh:mm DD/MM/YYYY")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "hh:mm dd/MM/yyyy")
     private LocalDateTime sendTime;
 }
